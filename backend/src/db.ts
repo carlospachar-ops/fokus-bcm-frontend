8edc843ba33654750292ee4850e5a3e9fac658de
barreranicolas-alt <barrera.nicolas@es.uazuay.edu.ec>
2026-02-24 17:17:41 -0500

    descripcion

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: "0512",
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT || 3306),
 });
